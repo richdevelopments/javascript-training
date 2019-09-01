f2fba23dfa0a5bcb12995714a84adcb7254a3a1c
Richard Wood <richardwood5@hotmail.com>
2019-09-01 14:51:19 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.write("Hello World!!");
+// alert("Hello from Javascript");
+// console.log("hello from Emily");
+
+    var apple = 45;
+    var orange = 30;
+    var banana = 50;
+
+var totalOfFruits = apple + orange + banana;
+document.write(totalOfFruits);
